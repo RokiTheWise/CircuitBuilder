d78c6fe7b788b21953d7254166802a3295561bca
RokiTheWise <dexterjethro.enriquez@gmail.com>
2026-02-11 01:56:54 +0800

    Fixed robots

--- logic-lens/app/robots.ts
+++ logic-lens/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://logisketch.com/sitemap.xml",
+    sitemap: "https://logisketch.djenriquez.dev/sitemap.xml",
   };
 }
